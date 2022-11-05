d8182432f7833acf655ad84f9a41fa7aabe546ac
paczaj-code <marcinpaczkowski@icloud.com>
2022-11-05 01:23:38 +0000

    Add temp Homepage component

---
+++ src/components/views/Homepage/Homepage.tsx
@@ -0,0 +1,200 @@
+import React from 'react';
+
+import { DatabaseStatisticsTypes } from '../../../types/initialDataTypes';
+import Section from '../../UI/Section/Section';
+
+const Homepage: React.FC<DatabaseStatisticsTypes> = ({
+  youtube_qty,
+  artists_qty,
+  track_qty,
+  release_qty,
+}) => {
+  return (
+    <Section section_type="homepage">
+      <p>
+        Sed magna commodo aliquip. Officia ut reprehenderit laboris cillum
+        dolore. Ut incididunt laborum ullamco cupidatat ut. Officia in velit
+        laboris aute ad. Dolor eiusmod esse commodo et Duis.
+      </p>
+      <p>
+        Tempor ut pariatur incididunt. Eiusmod nisi culpa est in. Et consequat
+        aute incididunt.
+      </p>
+      <p>
+        Non est officia aute nulla. Nostrud nulla non Lorem dolore. Laboris
+        fugiat cupidatat in. Consectetur Ut tempor do sed ullamco.
+      </p>
+      <p>
+        Ad tempor labore aute qui. Laborum ad aliqua aliqua anim ex. Nisi sit
+        nostrud aliquip esse. Nulla proident, dolor in.
+      </p>
+      <p>
+        Non ex qui occaecat labore. Dolor deserunt do laboris dolore eiusmod.
+        Deserunt sit eu nulla.
+      </p>
+      <p>
+        In sed tempor velit dolore. Cillum ex aliqua magna dolor pariatur. Anim
+        deserunt amet, ea.
+      </p>
+      <p>
+        Amet, dolor nostrud irure exercitation. Cupidatat et Ut nulla. Dolor
+        adipiscing nisi laboris.
+      </p>
+      <p>
+        Esse proident, esse Lorem Duis. Ad cillum dolor esse. Labore aliqua in
+        irure reprehenderit et.
+      </p>
+      <p>
+        Ea dolor sint voluptate nisi irure. Nulla tempor et ea minim. Duis
+        laboris laboris esse ut consequat.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+      <p>
+        Amet, culpa non quis in. Lorem culpa aute ut. Quis aute deserunt mollit
+        culpa ut. Ut sed commodo do.
+      </p>
+    </Section>
+  );
+};
+export default Homepage;
+
+//TODO refator i testy
