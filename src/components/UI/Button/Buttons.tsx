794badfd9a51494041df0b5672d054072a89b8e2
paczaj-code <marcinpaczkowski@icloud.com>
2022-11-05 01:20:31 +0000

    Add children prop

--- src/components/UI/Button/Buttons.tsx
+++ src/components/UI/Button/Buttons.tsx
@@ -4,19 +4,28 @@ interface ButtonTypes {
   type?: 'show-menu' | 'homepage';
   extra_class?: string;
   onClick?: React.MouseEventHandler;
+  children?: React.ReactNode;
 }
 
-const Button: React.FC<ButtonTypes> = ({ type, extra_class, onClick }) => {
+const Button: React.FC<ButtonTypes> = ({
+  type,
+  extra_class,
+  children,
+  onClick,
+}) => {
   return (
     <button
       aria-label="button"
       type="button"
       className={[
-        `button button--${type}`,
+        'button',
+        type && `button--${type}`,
         extra_class && `button--${extra_class}`,
       ].join(' ')}
       onClick={onClick}
-    ></button>
+    >
+      {children}
+    </button>
   );
 };
 
