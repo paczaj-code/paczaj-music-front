a83618acd6637d18c9758ff4898a9c52f64bedd0
paczaj-code <marcinpaczkowski@icloud.com>
2022-11-05 01:26:45 +0000

    Refactor component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { useContext, useState, useEffect } from 'react';
 import { AppContext } from './context/AppContext';
 import useAxios from './hooks/useAxios';
 import { InitialDataTypes } from './types/initialDataTypes';
-
 import Loader from './components/UI/Loader/Loader';
 import Sidebar from './components/Sidebar/Sidebar';
-// import ArtistsView from './components/organisms/ArtistsView/ArtistsView';
-// import Button from './components/UI/Button/Buttons';
-// import Homepage from './components/organisms/Homepage/Homepage';
-// import YoutubeModal from './components/molecules/YoutubeModal/YoutubeModal';
+import ArtistsView from './components/views/ArtistView/ArtistView';
+import Button from './components/UI/Button/Buttons';
+import Homepage from './components/views/Homepage/Homepage';
 
 const App = () => {
   const [initData, setInitData] = useState<InitialDataTypes | undefined>(
@@ -16,16 +14,10 @@ const App = () => {
   );
 
   const [showMenu, setShowMenu] = useState<boolean>(false);
-  // const [showStatistics, setShowStatistics] = useState<boolean>(true);
+  const [showHompage, setShowHompage] = useState<boolean>(true);
 
-  const {
-    loadingType,
-    setLoadingType,
-    // chosenArtistId,
-    // setChosenArtistId,
-    // chosenYoutubeId,
-    // setChosenYoutubeId,
-  } = useContext(AppContext);
+  const { loadingType, setLoadingType, chosenArtistId, setChosenArtistId } =
+    useContext(AppContext);
 
   const { isLoading } = useAxios(null, { url: 'init-data' }, setInitData, 1500);
 
@@ -36,31 +28,29 @@ const App = () => {
     }));
   }, [isLoading, setLoadingType]);
 
-  // useEffect(() => {
-  //   setShowMenu(false);
-  //   Boolean(chosenArtistId) && setShowStatistics(false);
-  // }, [chosenArtistId]);
+  useEffect(() => {
+    setShowMenu(false);
+    Boolean(chosenArtistId) && setShowHompage(false);
+  }, [chosenArtistId]);
 
-  // const showStatisticsHandler = () => {
-  //   setShowStatistics(true);
-  //   // setTimeout(() => {
-  //   setChosenArtistId(undefined);
-  //   // }, 500);
-  // };
+  const showHompageHandler = () => {
+    setShowHompage(true);
+    setChosenArtistId(undefined);
+  };
 
   return (
     <div className={['App', initData && 'App__data-loaded'].join(' ')}>
       <Loader trigger={loadingType?.application} />
       <>
-        {/* {initData && (
+        {initData && (
           <Button
             type="show-menu"
             onClick={() => setShowMenu((prev) => !prev)}
           />
         )}
         {chosenArtistId && (
-          <Button type="homepage" onClick={showStatisticsHandler} />
-        )} */}
+          <Button type="homepage" onClick={showHompageHandler} />
+        )}
         <div
           className={[
             'artist-list',
@@ -72,26 +62,16 @@ const App = () => {
         </div>
       </>
 
-      {/* {showStatistics && Boolean(initData) && (
+      {showHompage && Boolean(initData) && (
         <Homepage
           artists_qty={initData?.db_statistics?.artists_qty!}
           release_qty={initData?.db_statistics?.release_qty!}
           track_qty={initData?.db_statistics.track_qty!}
           youtube_qty={initData?.db_statistics.youtube_qty!}
         />
-      )} */}
+      )}
 
-      {/* {!showStatistics && chosenArtistId && <ArtistsView />} */}
-      {/* {chosenYoutubeId && (
-        <>
-          <Backdrop
-            type="youtube"
-            trigger={Boolean(chosenYoutubeId)}
-            onClick={() => setChosenYoutubeId(undefined)}
-          /> */}
-      {/* <YoutubeModal /> */}
-      {/* </> */}
-      {/* )} */}
+      {!showHompage && chosenArtistId && <ArtistsView />}
     </div>
   );
 };
