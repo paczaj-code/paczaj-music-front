32765982db98bac34cc2b8a77ff87c9cb3d76d92
paczaj-code <marcinpaczkowski@icloud.com>
2022-11-05 01:22:03 +0000

    Add data for tests

---
+++ src/components/views/__test__/data/testData.ts
@@ -0,0 +1,52 @@
+const wikipedia_description = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Sem viverra aliquet eget sit amet tellus cras. Suspendisse interdum consectetur 
+libero id faucibus nisl tincidunt. Neque laoreet suspendisse interdum consectetur libero id. 
+Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Nisl rhoncus mattis rhoncus urna neque viverra justo nec. 
+Mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Enim blandit volutpat maecenas volutpat blandit aliquam etiam. 
+Urna molestie at elementum eu. Morbi tristique senectus et netus.
+Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique. 
+Non tellus orci ac auctor augue. Dignissim convallis aenean et tortor at risus viverra adipiscing. 
+Aenean et tortor at risus viverra adipiscing. 
+Vulputate ut pharetra sit amet aliquam id diam. 
+Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque. 
+Purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus. 
+Est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat. 
+Amet facilisis magna etiam tempor orci eu lobortis. 
+Faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper. 
+Justo laoreet sit amet cursus sit amet. Facilisis volutpat est velit egestas dui id. 
+Facilisis magna etiam tempor orci eu lobortis. 
+Eget egestas purus viverra accumsan in nisl nisi scelerisque. 
+Morbi tempus iaculis urna id volutpat lacus laoreet.
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Sem viverra aliquet eget sit amet tellus cras. Suspendisse interdum consectetur 
+libero id faucibus nisl tincidunt. Neque laoreet suspendisse interdum consectetur libero id. 
+Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Nisl rhoncus mattis rhoncus urna neque viverra justo nec. 
+Mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Enim blandit volutpat maecenas volutpat blandit aliquam etiam. 
+Urna molestie at elementum eu. Morbi tristique senectus et netus.
+Venenatis a condimentum vitae sapien pellentesque habitant morbi tristique. 
+Non tellus orci ac auctor augue. Dignissim convallis aenean et tortor at risus viverra adipiscing. 
+Aenean et tortor at risus viverra adipiscing. 
+Vulputate ut pharetra sit amet aliquam id diam. 
+Volutpat maecenas volutpat blandit aliquam etiam erat velit scelerisque. 
+Purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus. 
+Est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat. 
+Amet facilisis magna etiam tempor orci eu lobortis. 
+Faucibus et molestie ac feugiat sed lectus vestibulum mattis ullamcorper. 
+Justo laoreet sit amet cursus sit amet. Facilisis volutpat est velit egestas dui id. 
+Facilisis magna etiam tempor orci eu lobortis. 
+Eget egestas purus viverra accumsan in nisl nisi scelerisque. 
+Morbi tempus iaculis urna id volutpat lacus laoreet.
+`;
+
+export const artistDetailsData = {
+  artist_name: 'Joy Division',
+  wikipedia_description: wikipedia_description,
+  wikipedia_link: 'JOY_DIVISION',
+  country_code: 'PL',
+  country: 'Poland',
+  begin_date_year: 1999,
+  end_date_year: 2002,
+  tags: ['indie', 'punk'],
+};
+
+// export const;
