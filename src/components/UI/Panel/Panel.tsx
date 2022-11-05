692b1ed7a7410cc4630631735b7a517a98d322ce
paczaj-code <marcinpaczkowski@icloud.com>
2022-11-05 01:21:06 +0000

    Add node refs

--- src/components/UI/Panel/Panel.tsx
+++ src/components/UI/Panel/Panel.tsx
@@ -3,12 +3,15 @@ import React from 'react';
 interface PanelTypes {
   children: React.ReactNode | React.ReactNode[];
   extra_class?: string;
+  nodeRef?: any;
 }
 
-const Panel: React.FC<PanelTypes> = ({ children, extra_class }) => {
-  return <div className={`panel panel--${extra_class}`}>{children}</div>;
+const Panel: React.FC<PanelTypes> = ({ children, extra_class, nodeRef }) => {
+  return (
+    <div ref={nodeRef} className={`panel panel__${extra_class}`}>
+      {children}
+    </div>
+  );
 };
 
 export default Panel;
-
-// TODO dodać testy
