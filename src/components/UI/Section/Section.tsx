9cb973927db5ea19bdb667ae6ef3a26673d9bb4d
paczaj-code <marcinpaczkowski@icloud.com>
2022-11-07 13:26:55 +0000

    Refactor component

--- src/components/UI/Section/Section.tsx
+++ src/components/UI/Section/Section.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import Heading from '../Heading/Heading';
+import Divider from '../../UI/Divider/Divider';
 
 interface SectionTypes {
   section_type: 'album' | 'youtube' | 'artists-details' | 'homepage';
+  title?: string;
+  title_icon?: string;
   children: React.ReactNode | React.ReactNode[];
 }
 
-const Section: React.FC<SectionTypes> = ({ children, section_type }) => {
+const Section: React.FC<SectionTypes> = ({
+  children,
+  section_type,
+  title,
+  title_icon,
+}) => {
   return (
-    <section className={`section section__${section_type}`}>{children}</section>
+    <section className={`section section__${section_type}`}>
+      {title && (
+        <>
+          <Heading
+            heading_type={section_type}
+            title={title}
+            title_icon={title_icon}
+          />
+          <Divider classes={section_type} />
+        </>
+      )}
+      {children}
+    </section>
   );
 };
 
